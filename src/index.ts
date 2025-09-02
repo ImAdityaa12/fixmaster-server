e38104530515eede3539951b43eefb295b3f8c36
ImAdityaa12 <adityagupta1291@gmail.com>
2025-09-02 14:22:01 +0530

    feat: initialize project with express and typescript

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello TypeScript + Express 🚀");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});

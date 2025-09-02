47c6572c35578f1e12568b811164fb5cc0e31367
ImAdityaa12 <adityagupta1291@gmail.com>
2025-09-02 20:53:47 +0530

    feat: Implement better-auth for user authentication and session management
    This commit introduces the `better-auth` library to manage user authentication and sessions. It includes:
    
    - Installation of `better-auth` and its dependencies.
    - Implementation of a Drizzle adapter for `better-auth` to interact with the database.
    - Creation of an authentication endpoint using `better-auth`.
    - Integration of CORS middleware to handle cross-origin requests.
    - Creation of `/api/me` route that is protected by authentication to test if the user is authenticated.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
+import 'dotenv/config'
 import express, { Request, Response } from "express";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./auth";
+import cors from "cors";
+import { fromNodeHeaders } from "better-auth/node";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL!, // Replace with your frontend's origin
+        methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+        credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    })
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 // Middleware
 app.use(express.json());
 
 // Routes
+
+app.get("/api/me", async (req, res) => {
+    const session = await auth.api.getSession({
+        headers: fromNodeHeaders(req.headers),
+    });
+    return res.json(session);
+});
+
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello TypeScript + Express 🚀");
 });
